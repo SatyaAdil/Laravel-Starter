16816d61bf9e44212adb49d395f244a6ab85b77e
SatyaAdil <adilfaishal11@gmail.com>
2025-05-21 19:37:44 +0700

    Update

--- database/migrations/2025_04_10_100620_create_categories_table.php
+++ database/migrations/2025_04_10_100620_create_categories_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->text('description')->nullable();
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('categories');
     }
 };
